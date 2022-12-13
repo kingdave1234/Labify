8da9129b3391639a90b3a89a1c93c9a3b2471215
kingdave1234 <davidaliu02@gmail.com>
2022-12-13 11:49:37 +0100

    New changes counter now works

--- counter.js
+++ counter.js
@@ -2,14 +2,14 @@ let section_counter = document.querySelector('#section_counter');
 let counters = document.querySelectorAll('.counter-item .counter');
 
 let speed = 200;
-counters.forEach((counter, index)=>{
+counters.forEach((counter, index) => {
     function UpdateCounter() {
-        const targetNumber = counter.dataset.target;
-        const initialNumber = counter.innerText;
+        const targetNumber = +counter.dataset.target;
+        const initialNumber = +counter.innerText;
         const incPerCount = targetNumber / speed;
-        if (initialNumber < targetNumber) {
-            counter.innerText = Math.ceil(initialNumber + incPerCount);
-            setTimeout(UpdateCounter,40);
+        if (initialNumber < targetNumber){
+            counter.innerText = initialNumber + incPerCount;
+            setTimeout(UpdateCounter, 40);
         }
     }
     UpdateCounter();
